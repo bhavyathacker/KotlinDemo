83b42bebcec0fe3e6b3a007c17fc90ef92cc76e9
Bhavya Thacker <bhavyathacker@gmail.com>
2022-06-18 20:24:04 +0530

    sample : Operators, String Template

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    var num1: Int = 5
+    var num2: Int = 7
+
+    var result = num1 + num2
+
+    println("The addition of $num1 and $num2 is $result")
+
+
+    var test = Person()
+
+    test.name = "Groot"
+
+    println("Person name is : ${test.name}")
+
+
+}
