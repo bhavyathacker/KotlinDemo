497d3c3df18e37257c5ac7aace671f574eb2ec75
Bhavya Thacker <bhavyathacker@gmail.com>
2022-06-19 11:10:05 +0530

    sample : if else expression

---
+++ src/main/kotlin/IfElseExpression.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var num1: Int = 10
+    var num2: Int = 5
+
+    var result: Int = 0
+
+    result = if (num1 > num2) {
+        num1
+    } else {
+        num2
+    }
+
+    println(result)
+
+}
