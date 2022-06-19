4840d40d34fcfd56eaa8dfb5f1a114090e31c749
Bhavya Thacker <bhavyathacker@gmail.com>
2022-06-19 20:39:40 +0530

    sample 21 : Try Expression

--- src/main/kotlin/StringToInteger.kt
+++ src/main/kotlin/StringToInteger.kt
@@ -1,3 +1,5 @@
+import java.lang.NumberFormatException
+
 // Example no: 20 String to Integer
 fun main() {
     var str: String = "4"
@@ -7,5 +9,18 @@ fun main() {
     num++
 
     println(num)
+
+    /*****************************************************************/
+    // Example no: 21 Try Expression
+    val str1: String = "4a"
+
+    var num2: Int = try {
+        str1.toInt()
+    } catch (e: NumberFormatException) {
+        -1
+    }
+
+    num2++
+    println(num2)
 
 }
