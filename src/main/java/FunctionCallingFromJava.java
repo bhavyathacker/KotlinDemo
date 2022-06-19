8865db332810bf395a2341016ebe4b48ee525aba
Bhavya Thacker <bhavyathacker@gmail.com>
2022-06-19 19:23:02 +0530

    sample 19 : Default and Named Parameters

--- src/main/java/FunctionCallingFromJava.java
+++ src/main/java/FunctionCallingFromJava.java
@@ -3,5 +3,11 @@ public class FunctionCallingFromJava {
 //        int add = FunctionExpressionKt.add(10, 25);// by default, Kotlin function is static we can call Kotlin function directly by calling class name
         int add = First.add(15, 25);// we have annotated our file name via @file:JvmName("First") so we can call same function
         System.out.println(add);
+
+        System.out.println(First.calcAmount(100, 0.08));
+
+        System.out.println(First.calcAmount(50));// we have annotated calcAmount method with @JvmOverloads, so we don't need to pass interest value
+
+
     }
 }
--- src/main/kotlin/FunctionExpression.kt
+++ src/main/kotlin/FunctionExpression.kt
@@ -1,6 +1,7 @@
 /*Example no: 17 Function Expression*/
 
-@file:JvmName("First")  //by writing this our class name will be changed from FunctionExpressionKt to First
+@file:JvmName("First")
+//by writing this our class name will be changed from FunctionExpressionKt to First
 
 fun main() {
 //    val result = add(5, 7)
@@ -8,6 +9,22 @@ fun main() {
     val result = max(15, 7)
 
     println(result)
+
+    val calcAmount = calcAmount(50, 0.04)
+    println("calculated amount with interest is : $calcAmount")
+
+    val calcAmount1 = calcAmount(60)
+    println("calculated amount with interest is : $calcAmount1")
+
+    /* we can call the same method by passing Named Parameters */
+    val calcAmount2 = calcAmount(amt = 70, interest = 0.05)
+    println("calculated amount with interest is : $calcAmount2")
+
+    /* Named Parameters can be passed in any sequence this is kotlin feature */
+    val calcAmount3 = calcAmount(interest = 0.05, amt = 80)
+    println("calculated amount with interest is : $calcAmount3")
+
+
 }
 
 /*fun add(a: Int, b: Int): Int {
@@ -17,3 +34,14 @@ fun main() {
 fun add(a: Int, b: Int): Int = a + b    //inline function
 
 fun max(a: Int, b: Int): Int = if (a > b) a else b  //we can use this a Ternary operator
+
+
+//Example no:19. Default and Named Parameters
+
+@JvmOverloads   // by this annotation kotlin will create two method with the same name: one with amount param and another with both the params
+fun calcAmount(
+    amt: Int,
+    interest: Double = 0.03 //we have passed default param value as 0.03 so even if we don't pass value for interest it will calc with default interest value
+): Int {
+    return (amt + amt * interest).toInt()
+}
